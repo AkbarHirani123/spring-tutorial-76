5a892e0cdb15742dd97bbc4005e7f1c633ff52f4
Akbar Hirani <akbar.hirani123@gmail.com>
2016-04-22 11:51:12 -0400

    Annotation configuration
    Changed XML configuration to annotation configuration Details in the
    book.

--- src/com/caveofprogramming/spring/aop/Logger.java
+++ src/com/caveofprogramming/spring/aop/Logger.java
@@ -5,15 +5,15 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
-//@Aspect
-//@Component
+@Aspect
+@Component
 public class Logger {
 
-	//@Pointcut("execution(void com.caveofprogramming.spring.aop.Camera.snap())")
+	@Pointcut("execution(void com.caveofprogramming.spring.aop.Camera.snap())")
 	public void cameraSnap() {
 		
 	}
-	//@Before("cameraSnap()")
+	@Before("cameraSnap()")
 	public void aboutToTakePhoto() {
 		System.out.println("About to take photo...");
 	}
