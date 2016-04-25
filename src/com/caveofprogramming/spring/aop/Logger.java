d321525f57716c01079bd18fff4f3f1c46536860
Akbar Hirani <akbar.hirani123@gmail.com>
2016-04-25 12:37:47 -0400

    Getting Arguments using Target and using Args
    “target(class)”
    ..
    .. SomePointCut(JoinPoint jp)
    jp.getArgs() = array of objects of the arguments passed to target method
    
    “arg(int e, double d)”
    ..(int e, double d)
    .. SomePointCut(int e, double d)
    then spring will match and find any argument of the same types passed

--- src/com/caveofprogramming/spring/aop/Logger.java
+++ src/com/caveofprogramming/spring/aop/Logger.java
@@ -1,5 +1,6 @@
 package com.caveofprogramming.spring.aop;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -9,13 +10,28 @@ import org.springframework.stereotype.Component;
 @Component
 public class Logger {
 
-	@Pointcut("args(int)")
-	public void somePointCut() {
+//	@Pointcut("target(com.caveofprogramming.spring.aop.Camera)")
+//	public void somePointCut() {
+//	}
+//
+//	@Before("somePointCut()")
+//	public void withinDemoAdvice(JoinPoint jp) {
+//		System.out.println("...Before Advice...");
+//		
+//		for(Object obj: jp.getArgs()) {
+//			System.out.println("ARG: "+obj);
+//		}
+//	}
+	
+	@Pointcut("args(exposure, aperture)")
+	public void somePointCut(int exposure, double aperture) {
 	}
 
-	@Before("somePointCut()")
-	public void withinDemoAdvice() {
+	@Before("somePointCut(exposure, aperture)")
+	public void withinDemoAdvice(int exposure, double aperture) {
 		System.out.println("...Before Advice...");
+		
+		System.out.printf("exposure %d, aperture %.2f\n", exposure, aperture);
 	}
 	
 }
